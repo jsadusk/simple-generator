1a47c50b5428cf2b10e99274de1b7dec5863edd7
Joe Sadusk <joe@sadusk.com>
2020-09-12 11:01:21 -0700

    Attempt at making Generator::Iterator Send so that you can par_bridge from it

--- src/generator.rs
+++ src/generator.rs
@@ -1,7 +1,6 @@
 pub trait Generator {
     type Item;
-    type Iterator: Iterator<Item=Self::Item>;
+    type Iterator: Iterator<Item = Self::Item> + Send;
 
     fn iter(&self) -> Self::Iterator;
 }
-
--- src/generator_func.rs
+++ src/generator_func.rs
@@ -1,27 +1,30 @@
 use crate::generator::*;
 
 pub struct GeneratorFunc<SetupFunc> {
-    setup: SetupFunc
+    setup: SetupFunc,
 }
 
 pub struct GeneratorFuncIterator<IterFunc> {
-    iterfunc: IterFunc
+    iterfunc: IterFunc,
 }
 
-impl<Value, IterFunc: FnMut() -> Option<Value>, SetupFunc: Fn() -> IterFunc> GeneratorFunc<SetupFunc> {
+impl<Value, IterFunc: FnMut() -> Option<Value>, SetupFunc: Fn() -> IterFunc>
+    GeneratorFunc<SetupFunc>
+{
     pub fn new(setup: SetupFunc) -> Self {
-        Self { setup: setup }
+        Self { setup }
     }
-    
 }
 
-impl<Value, IterFunc: FnMut() -> Option<Value>> GeneratorFuncIterator<IterFunc> {
+impl<Value, IterFunc: Send + FnMut() -> Option<Value>> GeneratorFuncIterator<IterFunc> {
     fn new(iterfunc: IterFunc) -> Self {
-        Self { iterfunc: iterfunc }
+        Self { iterfunc }
     }
 }
 
-impl<Value, IterFunc: FnMut() -> Option<Value>> std::iter::Iterator for GeneratorFuncIterator<IterFunc> {
+impl<Value, IterFunc: Send + FnMut() -> Option<Value>> std::iter::Iterator
+    for GeneratorFuncIterator<IterFunc>
+{
     type Item = Value;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -29,11 +32,13 @@ impl<Value, IterFunc: FnMut() -> Option<Value>> std::iter::Iterator for Generato
     }
 }
 
-impl<Value, IterFunc: FnMut() -> Option<Value>, SetupFunc: Fn() -> IterFunc> Generator for GeneratorFunc<SetupFunc> {
+impl<Value, IterFunc: Send + FnMut() -> Option<Value>, SetupFunc: Fn() -> IterFunc> Generator
+    for GeneratorFunc<SetupFunc>
+{
     type Iterator = GeneratorFuncIterator<IterFunc>;
     type Item = Value;
 
     fn iter(&self) -> Self::Iterator {
         GeneratorFuncIterator::new((self.setup)())
     }
-}    
+}
